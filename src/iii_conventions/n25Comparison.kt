a9149260243732251391ec7a1ac9e1db3ae3042b
新识 <fyj162016@alibaba-inc.com>
2018-12-24 13:02:20 +0800

    to #25

--- src/iii_conventions/n25Comparison.kt
+++ src/iii_conventions/n25Comparison.kt
@@ -15,8 +15,29 @@ fun todoTask25(): Nothing = TODO(
     references = { date: MyDate, comparable: Comparable<MyDate> -> }
 )
 
+/*
+operator fun MyDate.compareTo(other: MyDate) : Int {
+    if (year == other.year) {
+        if (month == other.month) {
+            return if (dayOfMonth == other.dayOfMonth) 0 else dayOfMonth - other.dayOfMonth
+        } else {
+            return month - other.month
+        }
+    }
+    return year - other.year
+}
+*/
+
+operator fun MyDate.compareTo(other: MyDate): Int = when {
+    year != other.year -> year - other.year
+    month != other.month -> month - other.month
+    else -> dayOfMonth - other.dayOfMonth
+}
+
+
+
 fun task25(date1: MyDate, date2: MyDate): Boolean {
-    todoTask25()
-//    return date1 < date2
+    //todoTask25()
+    return date1 < date2
 }
 
