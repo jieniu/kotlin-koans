ed408a077673482ec7987302b5731ea7196cac22
新识 <fyj162016@alibaba-inc.com>
2018-12-25 20:14:48 +0800

    finish iii_conventions

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,16 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private var numberInvoked: Int = 0) {
+    operator fun invoke(): Invokable {
+        numberInvoked++
+        return this
+    }
+
+    fun getNumberOfInvocations(): Int {
+        return numberInvoked;
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +23,6 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    //todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
